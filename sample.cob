63959f71c8fdfa6dd8d1b3e46724c29744d5def8
wharvex <t.gudlewski@gmail.com>
2024-12-24 21:55:31 -0500

    now it works

--- sample.cob
+++ sample.cob
@@ -1,17 +1,26 @@
-      *--- A simplified COBOL 85 program that will read employee disk
-      *--- records and create a printed payroll report containing the
-      *--- computed wages for each employee along with the input data.
-      *--- Adapted from Structured COBOL Programming 8ed by Stern/Stern.
+      ******************************************************************
+      * A simplified COBOL 85 program that will read employee disk     *
+      * records and create a printed payroll report containing the     *
+      * computed wages for each employee along with the input data.    *
+      * Adapted from Structured COBOL Programming 8ed by Stern/Stern.  *
+      ******************************************************************
+
        IDENTIFICATION DIVISION.
        PROGRAM-ID. SAMPLE.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
-       FILE-CONTROL.   SELECT EMPLOYEE-DATA    ASSIGN TO EMP-DAT.
-                       SELECT PAYROLL-LISTING  ASSIGN TO PRINTER.
+      * Input file name: `EMP-DAT`.
+      * Output file name: `PRINTER`.
+       FILE-CONTROL.   
+           SELECT EMPLOYEE-DATA ASSIGN TO EMP-DAT
+               ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT PAYROLL-LISTING ASSIGN TO PRINTER.
        DATA DIVISION.
        FILE SECTION.
-       FD  EMPLOYEE-DATA      LABEL RECORDS ARE STANDARD.
+       FD EMPLOYEE-DATA LABEL RECORDS ARE STANDARD.
        01  EMPLOYEE-RECORD.
+      * Fields within the record are on the 05 level, and as such are
+      * subordinate to (part of) the 01-level entry.
            05  EMPLOYEE-NAME-IN      PICTURE X(20).
            05  HOURS-WORKED-IN       PICTURE 9(2).
            05  HOURLY-RATE-IN        PICTURE 9V99.
@@ -31,6 +40,7 @@
        100-MAIN-MODULE.
            OPEN INPUT EMPLOYEE-DATA
                 OUTPUT PAYROLL-LISTING
+      *--- Repeatedly read one data record into the input area.
            PERFORM UNTIL ARE-THERE-MORE-RECORDS = 'NO '
                READ EMPLOYEE-DATA
                     AT END
