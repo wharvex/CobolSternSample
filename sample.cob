df62d4c23b3b33de55c4e3058bd6d83c2a35b1f8
wharvex <t.gudlewski@gmail.com>
2024-12-26 18:17:23 -0500

    loosen formatting

--- sample.cob
+++ sample.cob
@@ -3,60 +3,66 @@
       * records and create a printed payroll report containing the     *
       * computed wages for each employee along with the input data.    *
       * Adapted from Structured COBOL Programming 8ed by Stern/Stern.  *
+      * User-defined symbols are lower-case, COBOL keywords are UC.    *
       ******************************************************************
 
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. SAMPLE.
+       PROGRAM-ID. sample.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
-      * Input file name: `EMP-DAT`.
-      * Output file name: `PRINTER`.
+      * Input file name: `emp-dat`.
+      * Output file name: `printer`.
        FILE-CONTROL.   
-           SELECT EMPLOYEE-DATA ASSIGN TO EMP-DAT
+           SELECT employee-data ASSIGN TO emp-dat
+      *------- This line is important, and is not in the textbook.
+      *------- Without it, the GNUCobol compiler interprets newline
+      *------- characters as part of the data rows of a file.
                ORGANIZATION IS LINE SEQUENTIAL.
-           SELECT PAYROLL-LISTING ASSIGN TO PRINTER.
+           SELECT payroll-listing ASSIGN TO printer.
        DATA DIVISION.
        FILE SECTION.
-       FD EMPLOYEE-DATA LABEL RECORDS ARE STANDARD.
-       01  EMPLOYEE-RECORD.
+       FD employee-data LABEL RECORDS ARE STANDARD.
+       01 employee-record.
       * Fields within the record are on the 05 level, and as such are
       * subordinate to (part of) the 01-level entry.
-           05  EMPLOYEE-NAME-IN      PICTURE X(20).
-           05  HOURS-WORKED-IN       PICTURE 9(2).
-           05  HOURLY-RATE-IN        PICTURE 9V99.
-       FD  PAYROLL-LISTING    LABEL RECORDS ARE STANDARD.
-       01  PRINT-REC.
-           05                        PICTURE X(20).
-           05  NAME-OUT              PICTURE X(20).
-           05                        PICTURE X(10).
-           05  HOURS-OUT             PICTURE 9(2).
-           05                        PICTURE X(8).
-           05  RATE-OUT              PICTURE 9.99.
-           05                        PICTURE X(6).
-           05  WEEKLY-WAGES-OUT      PICTURE 999.99.
+           05 employee-name-in PICTURE X(20).
+           05 hours-worked-in PICTURE 9(2).
+           05 hourly-rate-in PICTURE 9V99.
+      * Use standard label records because it's not actually a printer.
+      * This also deviates from the textbook.
+       FD payroll-listing LABEL RECORDS ARE STANDARD.
+       01 print-rec.
+           05 PICTURE X(20).
+           05 name-out PICTURE X(20).
+           05 PICTURE X(10).
+           05 hours-out PICTURE 9(2).
+           05 PICTURE X(8).
+           05 rate-out PICTURE 9.99.
+           05 PICTURE X(6).
+           05 weekly-wages-out PICTURE 999.99.
        WORKING-STORAGE SECTION.
-       01  ARE-THERE-MORE-RECORDS    PICTURE XXX VALUE 'YES'.
+       01 are-there-more-records PICTURE XXX VALUE 'YES'.
        PROCEDURE DIVISION.
        100-MAIN-MODULE.
-           OPEN INPUT EMPLOYEE-DATA
-                OUTPUT PAYROLL-LISTING
+           OPEN INPUT employee-data
+               OUTPUT payroll-listing
       *--- Repeatedly read one data record into the input area.
-           PERFORM UNTIL ARE-THERE-MORE-RECORDS = 'NO '
-               READ EMPLOYEE-DATA
-                    AT END
-                         MOVE 'NO ' TO ARE-THERE-MORE-RECORDS
-                    NOT AT END
-                         PERFORM 200-WAGE-ROUTINE
+           PERFORM UNTIL are-there-more-records = 'NO '
+               READ employee-data
+                   AT END
+                       MOVE 'NO ' TO are-there-more-records
+                   NOT AT END
+                       PERFORM 200-WAGE-ROUTINE
                END-READ
            END-PERFORM
-           CLOSE EMPLOYEE-DATA
-                 PAYROLL-LISTING
+           CLOSE employee-data
+               payroll-listing
            STOP RUN.
        200-WAGE-ROUTINE.
-           MOVE SPACES TO PRINT-REC
-           MOVE EMPLOYEE-NAME-IN TO NAME-OUT
-           MOVE HOURS-WORKED-IN TO HOURS-OUT
-           MOVE HOURLY-RATE-IN TO RATE-OUT
-           MULTIPLY HOURS-WORKED-IN BY HOURLY-RATE-IN
-                    GIVING WEEKLY-WAGES-OUT
-           WRITE PRINT-REC.
+           MOVE SPACES TO print-rec
+           MOVE employee-name-in TO name-out
+           MOVE hours-worked-in TO hours-out
+           MOVE hourly-rate-in TO rate-out
+           MULTIPLY hours-worked-in BY hourly-rate-in
+               GIVING weekly-wages-out
+           WRITE print-rec.
